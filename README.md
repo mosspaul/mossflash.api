e1dc25f35c126741cde0dc94bf608cee1b27469e
mosspaul <gmosspaul18@gmail.com>
2026-03-10 22:11:31 -0600

    prep for server

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # mossflash.api
+
+
+## for adding database
+```sql
+CREATE DATABASE flashcards;
+
+CREATE USER flashcards_user WITH PASSWORD 'securepassword';
+
+GRANT ALL PRIVILEGES ON DATABASE flashcards TO flashcards_user;
+```
--- api/Program.cs
+++ api/Program.cs
@@ -3,10 +3,19 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+var host = Environment.GetEnvironmentVariable("DB_HOST");
+var db = Environment.GetEnvironmentVariable("DB_NAME");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var pass = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+var connectionString =
+    $"Host={host};Port=5432;Database={db};Username={user};Password={pass}";
+
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<MossFlashDbContext>(opt =>
-    opt.UseNpgsql(builder.Configuration.GetConnectionString("MossFlashDbConnectionString")));
+    opt.UseNpgsql(builder.Configuration.GetConnectionString(connectionString)));
 
 var app = builder.Build();
 app.MapControllers();
